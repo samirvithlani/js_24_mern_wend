501131c2c9e9d7a986fb8f872341c6e7f5e64c23
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-06 17:55:37 +0530

    array

---
+++ array/array1.js
@@ -0,0 +1,23 @@
+var users = ["amit","sumit","ajay","vijay"]
+
+//console.log(users[0])
+
+// for(let i=0;i<users.length;i++){
+    
+//         console.log(users[i])
+// }
+
+
+var l = users.push("rahul")
+l = users.unshift("raj")
+//console.log(l)
+console.log(users)
+var removedElm = users.pop()
+console.log(removedElm)
+
+removedElm = users.shift()
+console.log(removedElm)
+
+
+console.log(users)
+
